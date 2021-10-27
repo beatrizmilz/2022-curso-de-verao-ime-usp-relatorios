aeb956ddbdbbf2f1aecbc52ac97d2503d4d5d5f5
Beatriz Milz <beatriz.milz@hotmail.com>
2021-10-27 20:32:50 -0300

    atualiza pagina de profs

---
+++ info_profs.R
@@ -0,0 +1,46 @@
+library(magrittr)
+tibble::tribble(~ url_foto, ~nome, ~atividade,
+                ~url_github, ~url_twitter, ~url_facebook, ~url_site, ~mini_bio) %>%
+
+
+  # Infos BEA
+  tibble::add_row(
+    url_foto = "https://beatrizmilz.com/about/sidebar/avatar.jpeg",
+    nome = "Beatriz Milz",
+    atividade = "Docente",
+    url_github = "https://github.com/beatrizmilz",
+    url_twitter = "https://twitter.com/BeaMilz",
+    url_site = "https://beatrizmilz.com/",
+    mini_bio = "- Doutoranda em Ciência Ambiental na
+    Universidade de São Paulo (PROCAM/IEE/USP). <br>
+    - Instrutora de tidyverse certificada pela RStudio.<br>
+    - Co-organizadora da R-Ladies São Paulo, uma comunidade que tem como
+    objetivo promover a diversidade de gênero na comunidade da linguagem R. <br>
+    "
+  ) %>%
+
+  # Infos Carol
+
+  tibble::add_row(
+    url_foto = "https://pbs.twimg.com/profile_images/858423807022964736/x1Sjv9bJ_400x400.jpg",
+    nome = "Ana Carolina Moreno",
+    atividade = "Monitora",
+   # url_github = "",
+    url_twitter = "https://twitter.com/anarina",
+    url_facebook = "https://www.facebook.com/anacarolinamoreno/",
+    mini_bio = "Em breve" ) %>%
+
+  # Infos Tereza
+
+tibble::add_row(
+  url_foto = "https://avatars.githubusercontent.com/u/83192430?v=4",
+  nome = "Tereza Lacerda",
+  atividade = "Monitora",
+   url_github = "https://github.com/terezalacerda",
+  url_twitter = "https://twitter.com/tereza_lacerda",
+  mini_bio = "Em breve" ) %>%
+
+
+
+
+  readr::write_rds("info_profs.Rds")
